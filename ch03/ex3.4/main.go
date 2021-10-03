2d283494dfa83439342e48d9291d3fac2a951172
koukyo1994 <arabiannight1994@yahoo.co.jp>
2021-10-03 10:57:29 +0900

    color change

--- ch03/ex3.4/main.go
+++ ch03/ex3.4/main.go
@@ -2,34 +2,36 @@ package main
 
 import (
 	"fmt"
+	"image/color"
 	"io"
+	"log"
 	"math"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 const (
-	width, height = 600, 320            // キャンバスの大きさ (画素数)
-	cells         = 100                 // 格子のマス目の数
-	xyrange       = 30.0                // 軸の範囲 (-xyrange..+xyrange)
-	xyscale       = width / 2 / xyrange // x単位およびy単位あたりの画素数
-	zscale        = height * 0.4        // z単位あたりの画素数
-	angle         = math.Pi / 6         // x, y軸の角度 (30°)
+	MAX = 255
+	MIN = 0
 )
 
-var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
-
-func corner(i, j int) (float64, float64) {
+func corner(i, j int, cells float64, xyrange float64, width float64, height float64, xyscale float64, angle float64, zscale float64) (float64, float64, float64, float64) {
 	// マス目 (i, j)の角の点(x, y)を見つける。
 	x := xyrange * (float64(i)/cells - 0.5)
 	y := xyrange * (float64(j)/cells - 0.5)
 
 	// 面の高さzを計算する。
 	z := f(x, y)
+	r := math.Hypot(x, y)
+
+	cos := math.Cos(angle)
+	sin := math.Sin(angle)
 
 	// (x, y, z)を2-D SVGキャンバス(sx, sy)へ等角的に投影。
-	sx := width/2 + (x-y)*cos30*xyscale
-	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
-	return sx, sy
+	sx := width/2 + (x-y)*cos*xyscale
+	sy := height/2 + (x+y)*sin*xyscale - z*zscale
+	return sx, sy, z, r
 }
 
 func f(x, y float64) float64 {
@@ -37,16 +39,48 @@ func f(x, y float64) float64 {
 	return math.Sin(r) / r
 }
 
-func drawSurface(out io.Writer) {
+func decideColor(z, r float64) color.RGBA {
+	// zを0 - 255の範囲に変換する。
+	ztransformed := -(z*r - 1) / 2 * MAX
+	switch {
+	case ztransformed <= 60:
+		return color.RGBA{MAX, uint8(ztransformed/60*(MAX-MIN) + MIN), MIN, 255}
+	case 60 < ztransformed && ztransformed <= 120:
+		return color.RGBA{uint8((120-ztransformed)/60*(MAX-MIN) + MIN), MAX, MIN, 255}
+	case 120 < ztransformed && ztransformed <= 180:
+		return color.RGBA{MIN, MAX, uint8((ztransformed-120)/60*(MAX-MIN) + MIN), 255}
+	case 180 < ztransformed && ztransformed <= 240:
+		return color.RGBA{MIN, uint8((240-ztransformed)/60*(MAX-MIN) + MIN), MAX, 255}
+	case 240 < ztransformed:
+		return color.RGBA{uint8((ztransformed-240)/60*(MAX-MIN) + MIN), MIN, MAX, 255}
+	}
+	// ここには来ないはず。
+	return color.RGBA{MIN, MIN, MIN, 255}
+}
+
+func colorToString(c color.RGBA) string {
+	return fmt.Sprintf("rgb(%d,%d,%d)", c.R, c.G, c.B)
+}
+
+func isin(s string, a []string) bool {
+	for _, v := range a {
+		if v == s {
+			return true
+		}
+	}
+	return false
+}
+
+func drawSurface(out io.Writer, cells int, xyrange float64, width float64, height float64, xyscale float64, angle float64, zscale float64, colorStr string) {
 	fmt.Fprintf(out, "<svg xmlns='http://www.w3.org/2000/svg' "+
-		"style='stroke: grey; fill: white; stroke-width:0.7' "+
-		"width='%d' height='%d'>", width, height)
+		"style='stroke-width:0.7' "+
+		"width='%d' height='%d'>", int(width), int(height))
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
-			ax, ay := corner(i+1, j)
-			bx, by := corner(i, j)
-			cx, cy := corner(i, j+1)
-			dx, dy := corner(i+1, j+1)
+			ax, ay, az, ar := corner(i+1, j, float64(cells), xyrange, width, height, xyscale, angle, zscale)
+			bx, by, bz, br := corner(i, j, float64(cells), xyrange, width, height, xyscale, angle, zscale)
+			cx, cy, cz, cr := corner(i, j+1, float64(cells), xyrange, width, height, xyscale, angle, zscale)
+			dx, dy, dz, dr := corner(i+1, j+1, float64(cells), xyrange, width, height, xyscale, angle, zscale)
 
 			// NaNを除外するために、それぞれの点をチェックし、見つけた場合はcontinueする。
 			hasNaN := false
@@ -60,16 +94,99 @@ func drawSurface(out io.Writer) {
 				continue
 			}
 
-			fmt.Fprintf(out, "<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
-				ax, ay, bx, by, cx, cy, dx, dy)
+			// 色を決定する。
+			var fillColor string
+			if colorStr == "rainbow" {
+				fillColor = colorToString(decideColor((az+bz+cz+dz)/4, (ar+br+cr+dr)/4))
+			} else if strings.HasPrefix(colorStr, "#") {
+				c := color.RGBA{255, 255, 255, 255}
+				switch len(colorStr) {
+				case 7:
+					_, err := fmt.Sscanf(colorStr, "#%02x%02x%02x", &c.R, &c.G, &c.B)
+					if err != nil {
+						c = color.RGBA{255, 255, 255, 255}
+					}
+				case 4:
+					_, err := fmt.Sscanf(colorStr, "#%01x%01x%01x", &c.R, &c.G, &c.B)
+					if err != nil {
+						c = color.RGBA{255, 255, 255, 255}
+					}
+					c.R *= 17
+					c.G *= 17
+					c.B *= 17
+				}
+				fillColor = colorToString(c)
+			} else if isin(colorStr, []string{"black", "white", "red", "green", "blue", "yellow", "orange"}) {
+				fillColor = colorStr
+			} else {
+				fillColor = "white"
+			}
+
+			fmt.Fprintf(out, "<polygon points='%g,%g %g,%g %g,%g %g,%g' fill='%s'/>\n",
+				ax, ay, bx, by, cx, cy, dx, dy, fillColor)
 		}
 	}
 	fmt.Fprintln(out, "</svg>")
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	var (
+		width, height int     = 600, 320
+		cells         int     = 100
+		xyrange       float64 = 30
+		angle         float64 = math.Pi / 6
+		colorStr      string  = "#000000"
+	)
+	xyscale := float64(width) / 2 / xyrange
+	zscale := float64(height) * 0.4
+
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+	for k, v := range r.Form {
+		switch k {
+		case "width": // 横幅
+			if len(v) > 0 {
+				width_, _ := strconv.Atoi(v[0])
+				if width_ > 0 {
+					width = width_
+				}
+			}
+		case "height": // 縦幅
+			if len(v) > 0 {
+				height_, _ := strconv.Atoi(v[0])
+				if height_ > 0 {
+					height = height_
+				}
+			}
+		case "cells": // マス目
+			if len(v) > 0 {
+				cells_, _ := strconv.Atoi(v[0])
+				if cells_ > 0 {
+					cells = cells_
+				}
+			}
+		case "xyrange": // x, yの範囲
+			if len(v) > 0 {
+				xyrange_, _ := strconv.ParseFloat(v[0], 64)
+				if xyrange_ > 0 {
+					xyrange = xyrange_
+				}
+			}
+		case "angle": // 角度
+			if len(v) > 0 {
+				angle_, _ := strconv.ParseFloat(v[0], 64)
+				angle = math.Pi * angle_ / 180
+			}
+		case "color": // 色
+			if len(v) > 0 {
+				colorStr = v[0]
+			}
+		}
+	}
+
 	w.Header().Set("Content-Type", "image/svg+xml")
-	drawSurface(w)
+	drawSurface(w, cells, xyrange, float64(width), float64(height), xyscale, angle, zscale, colorStr)
 }
 
 func main() {
