ef042596916edc9bac7c584f11cf2ea8f3520fcc
koukyo1994 <arabiannight1994@yahoo.co.jp>
2021-10-10 11:38:54 +0900

    rev test

---
+++ ch04/rev/rev_test.go
@@ -0,0 +1,34 @@
+package rev_test
+
+import (
+	"testing"
+
+	"bootcamp/ch04/rev"
+)
+
+func TestReverse(t *testing.T) {
+	testcase := []struct {
+		input []int
+		want  []int
+	}{
+		{[]int{1, 2, 3}, []int{3, 2, 1}},
+		{[]int{1, 2, 3, 4}, []int{4, 3, 2, 1}},
+		{[]int{1, 2, 3, 4, 5}, []int{5, 4, 3, 2, 1}},
+	}
+
+	for _, c := range testcase {
+		in := c.input
+		rev.Reverse(in)
+
+		match := true
+		for i, v := range in {
+			if v != c.want[i] {
+				match = false
+				break
+			}
+		}
+		if !match {
+			t.Errorf("Reverse(%v) == %v, want %v", c.input, in, c.want)
+		}
+	}
+}
