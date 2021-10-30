1a42816e4003484f07bcada35c3f368c360b901a
koukyo1994 <arabiannight1994@yahoo.co.jp>
2021-10-30 21:06:54 +0900

    add geometry

---
+++ ch06/geometry/geometry.go
@@ -0,0 +1,33 @@
+package geometry
+
+import "math"
+
+type Point struct{ X, Y float64 }
+
+type Path []Point
+
+// 昔ながらの関数
+func Distance(p, q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+// 同じだが、Point型のメソッドとして
+func (p Point) Distance(q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+func (p *Point) ScaleBy(factor float64) {
+	p.X *= factor
+	p.Y *= factor
+}
+
+// Distanceはpathに沿って進んだ距離を返します
+func (path Path) Distance() float64 {
+	sum := 0.0
+	for i := range path {
+		if i > 0 {
+			sum += path[i-1].Distance(path[i])
+		}
+	}
+	return sum
+}
