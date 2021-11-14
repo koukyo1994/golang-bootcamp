2b34232e26777e8badf546f8bbaa63cd5e3c5e0e
koukyo1994 <arabiannight1994@yahoo.co.jp>
2021-11-14 13:44:13 +0900

    ex7.6

---
+++ ch07/ex7.6/tempconv/tempconv.go
@@ -0,0 +1,35 @@
+package tempconv
+
+import (
+	tc "bootcamp/ch02/tempconv"
+	"flag"
+	"fmt"
+)
+
+// *celsiusFlagはflag.Valueインターフェースを満足する
+// String()は既に定義されている
+type celsiusFlag struct{ tc.Celsius }
+
+func (f *celsiusFlag) Set(s string) error {
+	var unit string
+	var value float64
+	fmt.Sscanf(s, "%f%s", &value, &unit) // エラーは検査しない
+	switch unit {
+	case "C", "°C":
+		f.Celsius = tc.Celsius(value)
+		return nil
+	case "F", "°F":
+		f.Celsius = tc.FToC(tc.Fahrenheit(value))
+		return nil
+	case "K":
+		f.Celsius = tc.KToC(tc.Kelvin(value))
+		return nil
+	}
+	return fmt.Errorf("invalid temperature %q", s)
+}
+
+func CelsiusFlag(name string, value tc.Celsius, usage string) *tc.Celsius {
+	f := celsiusFlag{value}
+	flag.CommandLine.Var(&f, name, usage)
+	return &f.Celsius
+}
