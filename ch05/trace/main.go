e952197c59181123c5492f4e19ae7364d3b3feb5
koukyo1994 <arabiannight1994@yahoo.co.jp>
2021-11-19 00:06:48 +0900

    trace

---
+++ ch05/trace/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"time"
+)
+
+func trace(msg string) func() {
+	start := time.Now()
+	log.Printf("enter %s", msg)
+	return func() { log.Printf("exit %s (%s)", msg, time.Since(start)) }
+}
+
+func bigSlowOperation() {
+	defer trace("bigSlowOperation")()
+	time.Sleep(10 * time.Second)
+}
+
+func main() {
+	bigSlowOperation()
+}
