a18c717024c66eb789ecd83d405d0b0a785b336d
koukyo1994 <arabiannight1994@yahoo.co.jp>
2021-12-05 22:04:24 +0900

    8.13

---
+++ ch08/ex8.13/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"time"
+)
+
+type client chan<- string // 送信用メッセージチャンネル
+const timeout = 300
+
+var (
+	entering = make(chan client)
+	leaving  = make(chan client)
+	messages = make(chan string) // クライアントから受信する全てのメッセージ
+)
+
+func broadcaster() {
+	clients := make(map[client]bool) // 全ての接続されているクライアント
+	for {
+		select {
+		case msg := <-messages:
+			// 受信するメッセージを全てのクライアントの
+			// 送信用メッセージチャンネルへブロードキャストする
+			for cli := range clients {
+				cli <- msg
+			}
+		case cli := <-entering:
+			clients[cli] = true
+		case cli := <-leaving:
+			delete(clients, cli)
+			close(cli)
+		}
+	}
+}
+
+func handleConn(conn net.Conn) {
+	ch := make(chan string) // 送信用のクライアントメッセージ
+	go clientWriter(conn, ch)
+
+	who := conn.RemoteAddr().String()
+	ch <- "You are " + who
+	messages <- who + " has arrived"
+	entering <- ch
+
+	countdown := timeout
+	go func() {
+		ticker := time.NewTicker(1 * time.Second)
+		for countdown > 0 {
+			<-ticker.C
+			countdown--
+		}
+		fmt.Fprintf(conn, "you are in sleeping status for %d seconds\n", timeout)
+		ticker.Stop()
+		conn.Close()
+	}()
+
+	input := bufio.NewScanner(conn)
+	for input.Scan() {
+		messages <- who + ": " + input.Text()
+		countdown = timeout
+	}
+	// 注意: input.Err()からの潜在的なエラーを無視している
+
+	leaving <- ch
+	messages <- who + " has left"
+	conn.Close()
+}
+
+func clientWriter(conn net.Conn, ch <-chan string) {
+	for msg := range ch {
+		fmt.Fprintln(conn, msg) // 注意: ネットワークのエラーを無視している
+	}
+}
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go broadcaster()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
