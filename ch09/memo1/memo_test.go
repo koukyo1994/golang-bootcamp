af96d0dd7aaf3e907480bccffaddb4d839897d92
koukyo1994 <arabiannight1994@yahoo.co.jp>
2021-12-13 08:29:43 +0900

    memo

---
+++ ch09/memo1/memo_test.go
@@ -0,0 +1,87 @@
+package memo_test
+
+import (
+	memo "bootcamp/ch09/memo1"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"sync"
+	"testing"
+	"time"
+)
+
+func httpGetBody(url string) (interface{}, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
+}
+
+func incomingURLS() []string {
+	return []string{
+		"https://golang.org",
+		"https://godoc.org",
+		"https://play.golang.org",
+		"http://gopl.io",
+	}
+}
+
+func TestMemo(t *testing.T) {
+	m := memo.New(httpGetBody)
+	for _, url := range incomingURLS() {
+		start := time.Now()
+		value, err := m.Get(url)
+		if err != nil {
+			log.Print(err)
+		}
+		fmt.Printf("%s, %s, %d bytes\n",
+			url, time.Since(start), len(value.([]byte)))
+	}
+	fmt.Println()
+	for _, url := range incomingURLS() {
+		start := time.Now()
+		value, err := m.Get(url)
+		if err != nil {
+			log.Print(err)
+		}
+		fmt.Printf("%s, %s, %d bytes\n",
+			url, time.Since(start), len(value.([]byte)))
+	}
+}
+
+func TestMemoConcurrent(t *testing.T) {
+	m := memo.New(httpGetBody)
+	var n sync.WaitGroup
+	for _, url := range incomingURLS() {
+		n.Add(1)
+		go func(url string) {
+			start := time.Now()
+			value, err := m.Get(url)
+			if err != nil {
+				log.Print(err)
+			}
+			fmt.Printf("%s, %s, %d bytes\n",
+				url, time.Since(start), len(value.([]byte)))
+			n.Done()
+		}(url)
+	}
+	n.Wait()
+	fmt.Println()
+	for _, url := range incomingURLS() {
+		n.Add(1)
+		go func(url string) {
+			start := time.Now()
+			value, err := m.Get(url)
+			if err != nil {
+				log.Print(err)
+			}
+			fmt.Printf("%s, %s, %d bytes\n",
+				url, time.Since(start), len(value.([]byte)))
+			n.Done()
+		}(url)
+	}
+	n.Wait()
+}
