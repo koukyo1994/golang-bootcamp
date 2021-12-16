3ba623a505b9f12ea8b4241166e833292ffe27f9
koukyo1994 <arabiannight1994@yahoo.co.jp>
2021-12-17 00:01:24 +0900

    7.17

--- ch07/ex7.17/main.go
+++ ch07/ex7.17/main.go
@@ -4,13 +4,36 @@ import (
 	"encoding/xml"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"strings"
 )
 
 func main() {
-	dec := xml.NewDecoder(os.Stdin)
+	url := os.Args[1]
+	var elements, attributes string
+	attrs := make(map[string]string)
+
+	fmt.Printf("element names: \n")
+	fmt.Scanln(&elements)
+	names := strings.Split(elements, ",")
+
+	fmt.Printf("attributes: \n")
+	fmt.Scanln(&attributes)
+	splittedAttributes := strings.Split(attributes, ",")
+	for _, attr := range splittedAttributes {
+		keyValue := strings.Split(attr, ":")
+		if len(keyValue) != 2 {
+			fmt.Printf("invalid attribute format: %v\n", attr)
+		} else {
+			attrs[keyValue[0]] = keyValue[1]
+		}
+	}
+
+	dec := xml.NewDecoder(strings.NewReader(fetch(url)))
 	var stack []string
+	var attrsMap map[string]string
 	for {
 		tok, err := dec.Token()
 		if err == io.EOF {
@@ -22,10 +45,15 @@ func main() {
 		switch tok := tok.(type) {
 		case xml.StartElement:
 			stack = append(stack, tok.Name.Local) // push
+			attrsMap = make(map[string]string)    // 新しい開始要素が来たら初期化
+			for _, a := range tok.Attr {
+				attrsMap[a.Name.Local] = a.Value
+			}
 		case xml.EndElement:
 			stack = stack[:len(stack)-1] // pop
 		case xml.CharData:
-			if containsAll(stack, os.Args[1:]) {
+			// fmt.Printf("%v %v\n", stack, attrsMap)
+			if containsAll(stack, names) && attributesMatch(attrsMap, attrs) {
 				fmt.Printf("%s: %s\n", strings.Join(stack, " "), tok)
 			}
 		}
@@ -43,4 +71,28 @@ func containsAll(x, y []string) bool {
 		x = x[1:]
 	}
 	return false
+}
+
+func attributesMatch(x, y map[string]string) bool {
+	for k, v := range y {
+		if x[k] != v {
+			return false
+		}
+	}
+	return true
+}
+
+func fetch(url string) string {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+		os.Exit(1)
+	}
+	return string(b)
 }
